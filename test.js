ec1a80ba931759f2891eb15522725168d56166a9
piyapangithub <piyapan.m@gli.co.th>
2022-07-25 21:08:18 +0700

    update test.js

--- test.js
+++ test.js
@@ -5,8 +5,8 @@ const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
   .usingServer('http://35.198.193.61:4444/wd/hub')
   .build();
   try {
-    await driver.get('http://www.google.com/');
-    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+    await driver.get('http://35.198.193.61:3000/');
+    await driver.findElement(By.name('value')).sendKeys('GLI1', Key.RETURN);
     //await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
   } finally {
     await driver.quit();
