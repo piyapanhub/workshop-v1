64ebd404e25365fcc78747b9c6efd5d049c25616
piyapangithub <piyapan.m@gli.co.th>
2022-07-25 21:11:17 +0700

    update index and test.js

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
   .build();
   try {
     await driver.get('http://35.198.193.61:3000/');
-    await driver.findElement(By.name('value')).sendKeys('GLI1', Key.RETURN);
+    await driver.findElement(By.name('value')).sendKeys('GLI2', Key.RETURN);
     //await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
   } finally {
     await driver.quit();
