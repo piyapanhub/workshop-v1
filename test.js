f85c0358855208253ad11557c0f604c66816bf03
piyapangithub <piyapan.m@gli.co.th>
2022-07-25 20:18:47 +0700

    update

---
+++ test.js
@@ -0,0 +1,14 @@
+const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
+
+(async function example() {
+  let driver = await new Builder().forBrowser(Browser.CHROME)
+  .usingServer('http://34.126.167.224:4444/wd/hub')
+  .build();
+  try {
+    await driver.get('http://www.google.com/');
+    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+    //await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+  } finally {
+    await driver.quit();
+  }
+})();
